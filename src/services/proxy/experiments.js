a8c41a44eb1e03dc253c342c107fe71a32e7f8d8
Manos Angelidis <eangelid@epfl.ch>
2020-11-02 17:18:49 +0100

    [NRRPLT-8002] Added jsdoc style documentation

--- src/services/proxy/experiments.js
+++ src/services/proxy/experiments.js
@@ -2,7 +2,19 @@ import { options } from '../httpRequestOptions.js'
 import endpoints from './data/endpoints.json'
 
 
+/**
+* Service that fetches the template experiments list from the proxy given
+* that the user has authenticated successfully.
+*/
 class ExperimentsService {
+
+    /**
+    * Retrieves the list of template experiments from the proxy and stores 
+    * them in the experiments class property. If the experiments are already
+    * there it just returns them, else does an HTTP request. 
+    * @return {experiments} the list of template experiments
+    */
+    //TODO remove token when the authentication class is there
     getExperiments = async token => {
         if (this.experiments) {
             return this.experiments;
