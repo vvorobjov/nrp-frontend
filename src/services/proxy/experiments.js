7016277f16ebd6e0f3e1ba7b7a600c427f3b1642
Manos Angelidis <eangelid@epfl.ch>
2020-11-02 18:07:15 +0100

    [NRRPLT-8002] Refactored the http requests file to a generic http request class
    - New generic http request class that all the proxy and backend services can use
      to perform requests
    
    - Experiments service class now uses the proxy url from the config.json

--- src/services/proxy/experiments.js
+++ src/services/proxy/experiments.js
@@ -1,12 +1,12 @@
-import { options } from '../httpRequestOptions.js'
 import endpoints from './data/endpoints.json'
-
+import { httpRequest } from '../httpService.js';
+import config from '../../config.json'
 
 /**
 * Service that fetches the template experiments list from the proxy given
 * that the user has authenticated successfully.
 */
-class ExperimentsService {
+class ExperimentsService extends httpRequest {
 
     /**
     * Retrieves the list of template experiments from the proxy and stores 
@@ -20,13 +20,8 @@ class ExperimentsService {
             return this.experiments;
         }
         const proxyEndpoint = endpoints.proxy;
-        const experimentsUrl = `${proxyEndpoint.url}${proxyEndpoint.experiments.url}`;
-
-        // Add authorization header
-        options.headers.Authorization = `Bearer ${token}`;
-
-        const response = await fetch(experimentsUrl, options);
-        this.experiments = await response.json();
+        const experimentsUrl = `${config.api.proxy.url}${proxyEndpoint.experiments.url}`;
+        this.experiments = await this.get(experimentsUrl, token);
         return this.experiments;
     }
 }
