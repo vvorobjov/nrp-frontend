24f1ff4e6532f20df7cbc5c07b8f74dec7332b0f
Antoine Detailleur <detailleur@fortiss.org>
2020-11-16 13:00:14 +0100

    [NRRPLT-8003] Experiment, ExperimentList and ExperimentContainer created

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import React from 'react';
 import experimentsService from './services/proxy/experiments.js';
 
 import EntryPage from "./components/entry-page/entry-page.js";
+import ExperimentContainer from './components/experiment-list/experiment-list.js';
 
 class App extends React.Component {
   async componentDidMount() {
     // replace the token here with a token found in your database in ~/.opt/nrpStorage/FS_db/users for testing
     try {
-      const experiments = await experimentsService.getExperiments('42bbb566-01a0-41f0-938e-c15c5722fab7');
+      const experiments = await experimentsService.getExperiments('27f3988c-ccfe-4519-921c-b804a966d708');
       console.log(experiments);
     }
     catch (error) {
@@ -16,7 +17,14 @@ class App extends React.Component {
     }
   };
 
-  render() {return <EntryPage />;}
+  render() {
+    return (
+      <React.Fragment>
+        <EntryPage />
+        <ExperimentContainer />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
