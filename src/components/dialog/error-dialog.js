e4661f393e9c8d3f7c708dbdc47c958ab04e94e6
Sandro Weber <webers@in.tum.de>
2021-03-30 16:12:55 +0200

    css-ing

--- src/components/dialog/error-dialog.js
+++ src/components/dialog/error-dialog.js
@@ -41,38 +41,40 @@ class ErrorDialog extends React.Component{
   render(){
     let error = this.state.error;
     return (
-      <div className="error-dialog-overlay-wrapper">
+      <div>
         { this.state.error ?
-          <Modal.Dialog className="modal-dialog">
-            <Modal.Header>
-              <h4>{error.type}</h4>
-            </Modal.Header>
-            <Modal.Body>
-              <p>{error.message}</p>
-            </Modal.Body>
-            <Modal.Footer>
-              <div>
-                <Button variant="outline-dark" onClick={this.handleClose()}>
-                  <span className="glyphicon glyphicon-remove"></span> Close
-                </Button>
-                <Button variant="outline-dark" className="pull-right" /*onClick={this.sourceDisplay()}*/>
-                  {this.state.isErrorSourceDisplayed ? 'Hide' : 'Show'} scary details <span className="caret"></span>
-                </Button>
-              </div>
-              {!this.state.isErrorSourceDisplayed
-                ?<div>
-                  <h5>Error Type</h5><pre>{error.type}</pre>
-                  <h6>Error Code</h6><pre>{error.code}</pre>
-                  <h6>Message</h6><pre>{error.message}</pre>
-                  {error.data
-                    ? <div><h6>Data</h6><pre>{error.data}</pre></div>
-                    : null}
-                  <div><h6>Stack Trace</h6><pre>{error.stack}</pre></div>
+          <div className="modal-dialog-wrapper" onClick={event => event.stopPropagation()}>
+            <Modal.Dialog className="modal-dialog">
+              <Modal.Header>
+                <h4>{error.type}</h4>
+              </Modal.Header>
+              <Modal.Body>
+                <p>{error.message}</p>
+              </Modal.Body>
+              <Modal.Footer>
+                <div>
+                  <Button variant="outline-dark" onClick={this.handleClose()}>
+                    <span className="glyphicon glyphicon-remove"></span> Close
+                  </Button>
+                  <Button variant="outline-dark" className="pull-right" /*onClick={this.sourceDisplay()}*/>
+                    {this.state.isErrorSourceDisplayed ? 'Hide' : 'Show'} scary details <span className="caret"></span>
+                  </Button>
                 </div>
-                : null
-              }
-            </Modal.Footer>
-          </Modal.Dialog>
+                {!this.state.isErrorSourceDisplayed
+                  ?<div>
+                    <h5>Error Type</h5><pre>{error.type}</pre>
+                    <h6>Error Code</h6><pre>{error.code}</pre>
+                    <h6>Message</h6><pre>{error.message}</pre>
+                    {error.data
+                      ? <div><h6>Data</h6><pre>{error.data}</pre></div>
+                      : null}
+                    <div><h6>Stack Trace</h6><pre>{error.stack}</pre></div>
+                  </div>
+                  : null
+                }
+              </Modal.Footer>
+            </Modal.Dialog>
+          </div>
           : null}
       </div>
     );
