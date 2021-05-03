e42e2c862d22a82c117cc9d1d9231063fe701b41
Sandro Weber <webers@in.tum.de>
2021-05-03 12:31:31 +0200

    experiment list formatting errors fixed?

--- src/components/experiment-list/experiment-list.js
+++ src/components/experiment-list/experiment-list.js
@@ -8,22 +8,20 @@ export default class ExperimentList extends React.Component {
   render() {
     return (
       <div className='experiment-list-wrapper'>
-        <div className='experiment-list'>
-          {this.props.experiments.length === 0 ?
-            <div className='no-items-notification'>List is currently empty ...</div> :
-            <ol>
-              {this.props.experiments.map(experiment => {
-                return (
-                  <li key={experiment.id || experiment.configuration.id} className='nostyle'>
-                    <ExperimentListElement experiment={experiment}
-                      availableServers={this.props.availableServers}
-                      startingExperiment={this.props.startingExperiment}
-                      selectExperimentOverviewTab={this.props.selectExperimentOverviewTab} />
-                  </li>
-                );
-              })}
-            </ol>}
-        </div>
+        {this.props.experiments.length === 0 ?
+          <div className='no-items-notification'>List is currently empty ...</div> :
+          <ol>
+            {this.props.experiments.map(experiment => {
+              return (
+                <li key={experiment.id || experiment.configuration.id} className='nostyle'>
+                  <ExperimentListElement experiment={experiment}
+                    availableServers={this.props.availableServers}
+                    startingExperiment={this.props.startingExperiment}
+                    selectExperimentOverviewTab={this.props.selectExperimentOverviewTab} />
+                </li>
+              );
+            })}
+          </ol>}
       </div>
     );
   }
