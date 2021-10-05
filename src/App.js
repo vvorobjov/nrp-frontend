9adddfba541a2e1e3ccf984b0dc32e1fa90a2c18
Sandro Weber <webers@in.tum.de>
2021-10-05 08:26:35 +0200

    working together with correct mqtt and nginx config

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-
 import { HashRouter, Switch, Route } from 'react-router-dom';
+
+import mqtt from 'mqtt';
 
 import EntryPage from './components/entry-page/entry-page';
 import ErrorDialog from './components/dialog/error-dialog.js';
@@ -9,6 +10,25 @@ import SimulationView from './components/simulation-view/simulation-view';
 import NotificationDialog from './components/dialog/notification-dialog.js';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    console.info(window.location);
+    const client = mqtt.connect('ws://' + window.location.hostname + ':9000/mqtt');
+    client.on('connect', () => {
+      console.info('MQTT connected');
+      console.info(client);
+    });
+    client.on('error', (error) => {
+      console.error(error);
+    });
+    client.on('message', (topic, payload, packet) => {
+      console.info('MQTT message');
+      console.info(topic);
+      console.info(payload);
+      console.info(packet);
+    });
+  }
+
   render() {
     return (
       <div>
