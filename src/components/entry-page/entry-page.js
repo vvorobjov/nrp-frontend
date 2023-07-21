6f125c18c26b9f1f014b74189c26237380d4a686
Michael Neumeier <neumeier@fortiss.org>
2023-07-21 07:48:48 +0000

    Merged in NRRPLT-8831 (pull request #53)
    NRRPLT-8831
    
    * [NRRPLT-8831] gui changes
    
    * Merge remote-tracking branch 'origin/development' into NRRPLT-8831
    
    * Merge remote-tracking branch 'origin/development' into NRRPLT-8831
    
    * [NRRPLT-8831] remove unnecessary imports
    
    * mini layout change
    
    * [NRRPLT-8831] move cookie-storage to experiment-workbench, save only ID
    
    * [NRRPLT-8831] save and display last experiment list
    
    * beauty adjustments
    
    * [NRRPLT-8831] delete and shutdown experiments from entry page
    
    * little cleanup
    
    
    Approved-by: Sandro Weber

--- src/components/entry-page/entry-page.js
+++ src/components/entry-page/entry-page.js
@@ -1,42 +1,93 @@
 import React from 'react';
 
+import { instanceOf } from 'prop-types';
+import { Cookies } from 'react-cookie';
+import { Link } from 'react-router-dom';
+
 import NrpHeader from '../nrp-header/nrp-header.js';
 
 import './entry-page.css';
 import NrpCoreDashboard from '../nrp-core-dashboard/nrp-core-dashboard.js';
-//import TransceiverFunctionEditor from '../tf-editor/tf-editor';
+import ExperimentListElement from '../experiment-list/experiment-list-element.js';
+import ExperimentStorageService from '../../services/experiments/files/experiment-storage-service';
 
 export default class EntryPage extends React.Component {
+
+  static propTypes = {
+    cookies: instanceOf(Cookies).isRequired
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      experimentIDs: [],
+      experimentInfos: []
+    };
+  }
+
+  async componentDidMount() {
+    this.onUpdateStorageExperiments = this.onUpdateStorageExperiments.bind(this);
+    ExperimentStorageService.instance.addListener(
+      ExperimentStorageService.EVENTS.UPDATE_EXPERIMENTS,
+      this.onUpdateStorageExperiments
+    );
+  }
+
+  onUpdateStorageExperiments(storageExperiments) {
+    this.getLastExperiments(storageExperiments);
+  }
+
+  getLastExperiments(storageExperiments){
+    var experimentIDs = undefined;
+    // get IDs from cookie
+    experimentIDs = this.props.cookies.get('experimentIDs');
+    if (experimentIDs){
+      this.setState({experimentIDs: experimentIDs});
+    }
+    var experimentInfos = [];
+    var experimentInfo = undefined;
+    // get experiement infos if ID in storageExperiments
+    this.state.experimentIDs.forEach((expID) =>{
+      experimentInfo = storageExperiments.find(experiment => experiment.id === expID);
+      if (experimentInfo){
+        experimentInfos = [...experimentInfos, experimentInfo];
+      }
+    });
+    this.setState({experimentInfos: experimentInfos});
+  }
+
+
   render() {
     return (
       <div className='entry-page-wrapper'>
         <div className='entry-page-header'>
           <NrpHeader title1='NEUROROBOTICS' title2='PLATFORM' />
         </div >
-        {/* <div className='sidebar-left'></div>
-        <div className='experiments-left'>
-          <img
-            src={PlaceholderImage}
-            alt='Experiment List'
-            className='img-experiment-list'
-          />
-          <p>see the full list of experiments</p>
-        </div>
-        <div className='experiments-right'>
-          <h3>Most recent</h3>
-          <p>Experiment 1 Placeholder</p>
-          <p>Experiment 2 Placeholder</p>
-        </div>
-        <div className='sidebar-right'></div>*/}
-        {/* <div>
-          <div><b>!!! NRP Core testing !!!</b></div>
-        </div> */}
-        <div className='nrp-core-dashboard sidebar-left'>
+        <div className='dashboard'>
           <NrpCoreDashboard />
         </div >
-        <iframe title='neurorobotics-news' className='nrp-news sidebar-right'
-          src='https://neurorobotics.net/latest.html' />
-        {/*<TransceiverFunctionEditor experimentId='mqtt_simple_1'/>*/}
+        <div className='experiments'>
+          <h4>Last Experiments:</h4>
+          {(this.state.experimentInfos.length > 0) ?
+            <ol>
+              {this.state.experimentInfos.map(experiment => {
+                return (
+                  <li key={experiment.id || experiment.configuration.id} className='no-style'>
+                    <ExperimentListElement experiment={experiment}
+                      availableServers={experiment.joinableServers} />
+                  </li>
+                );
+              })}
+            </ol>
+            :
+            <Link to='/experiments-overview'
+              className='first-experiment-link'>
+              Start your first experiment!
+            </Link>
+          }
+        </div>
+        <iframe title='nrp-news' className='news' src='https://neurorobotics.net/latest.html' />
       </div>
     );
   }
